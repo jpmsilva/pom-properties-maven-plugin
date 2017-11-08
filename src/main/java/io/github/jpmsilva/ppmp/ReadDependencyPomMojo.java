39be4ae3b49c6bf4f6b59f4ad2cc4b913d3f5b2b
jpmsilva <30354367+jpmsilva@users.noreply.github.com>
2017-11-08 23:24:07 +0000

    Added site.

--- src/main/java/io/github/jpmsilva/ppmp/ReadDependencyPomMojo.java
+++ src/main/java/io/github/jpmsilva/ppmp/ReadDependencyPomMojo.java
@@ -1,5 +1,3 @@
-package io.github.jpmsilva.ppmp;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -18,6 +16,8 @@ package io.github.jpmsilva.ppmp;
  * specific language governing permissions and limitations
  * under the License.
  */
+
+package io.github.jpmsilva.ppmp;
 
 import static org.apache.maven.plugins.annotations.LifecyclePhase.INITIALIZE;
 import static org.apache.maven.plugins.annotations.ResolutionScope.COMPILE_PLUS_RUNTIME;
